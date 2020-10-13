f85cae91dedd68c8e11785383c1941c7859ef578
Юлия <72439922+13SavinaJulia@users.noreply.github.com>
2020-10-13 15:49:54 +0900

    Initial commit

---
+++ From.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionFile1
+{
+    public class From
+    {
+        /// <summary>
+        /// Функция выводит приветствие студента 
+        /// </summary>
+        /// <param name="name"></param>
+        public void HelloWord(string name)
+        {
+            Console.WriteLine($"Здравствуйте, Вы студент педагогического колледжа, ваше имя-, {name}");
+
+        }
+
+        /// <summary>
+        /// Функция считает в каком году студент окончит колледж
+        /// </summary>
+        /// <param name="age"></param>
+        /// <returns></returns>
+        public int AgeToFive(int age)
+        {
+            return age + 4;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# FunctionFile1
+ 
